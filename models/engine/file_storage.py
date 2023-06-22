49b7798299a436f23b03396239d063173d751e17
degree2466 <uzochukwugodwin959@gmail.com>
2023-06-21 21:01:28 -0700

    task 1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,7 @@
 import json
 
 
-class FileStorage:
+class HBNBCommand:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
@@ -25,8 +25,9 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
-    def reload(self):
+def reload(self):
         """Loads storage dictionary from file"""
+        import unittest
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -48,3 +49,32 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+class HBNBCommand(unittest.TestCase):
+    def setUp(self):
+        self.hbnb = HBNBCommand()
+        self.storage = HBNBCommand()
+        self.hbnb.storage = self.storage
+
+    def tearDown(self):
+        self.storage._FileStorage__objects = {}
+
+    # Add your tests here
+    
+    def test_do_create_with_parameters(self):
+        # Test with string and integer parameters
+        self.assertFalse(self.storage.all())  # Ensure storage is empty
+
+        # Create an object of the class with string and integer parameters
+        self.hbnb.do_create("MyClass string_param=\"Hello\" int_param=123")
+
+        # Check if the object was created and saved
+        objects = self.storage.all()
+        self.assertEqual(len(objects), 1)  # Expecting 1 object in storage
+        obj = list(objects.values())[0]
+        self.assertEqual(obj.__class__.__name__, "MyClass")  # Object class should match
+        self.assertEqual(obj.string_param, "Hello")  # string_param should match
+        self.assertEqual(obj.int_param, 123)  # int_param should match
+
+    if __name__ == '__main__':
+        unittest.main()
